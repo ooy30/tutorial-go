1b4b20c3f58ae0e7a0bf8256f7cd1d21115233f0
Ooy <suwananpoedpong@gmail.com>
2022-03-08 23:09:50 +0700

    [UPDATE]

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	entity.SetupDatabase()
 	r := gin.Default()
+	r.Use(CORSMiddleware())
 	// User Routes
 	r.GET("/users", controller.ListUsers)
 	r.GET("/user/:id", controller.GetUser)
@@ -17,4 +18,18 @@ func main() {
 	r.DELETE("/users/:id", controller.DeleteUser)
 	// Run the server
 	r.Run()
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
 }
